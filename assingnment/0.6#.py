b9b9e27e4cc184391c629ad3b36cd3a21921f67d
o-yosufi <obaidullahyosafi@gmail.com>
2023-07-20 15:39:16 +0500

    add new assignment

---
+++ assingnment/0.6#.py
@@ -0,0 +1,12 @@
+year = 1
+days = 365
+hours = 24
+minutes = 60
+seconds = 60 
+x = "hours in a year"
+y = "minutes in a year"
+z = "seconds in a year"
+
+print((x ), year * days * hours)
+print((y), year * days * hours * minutes)
+print((z), year * days * hours * minutes * seconds)
---
+++ python/0.6#.py
@@ -0,0 +1,7 @@
+year = 1
+days = 365
+hours = 24
+minutes = 60
+seconds = 60 
+
+print(hours in year,(year * days * hours))
