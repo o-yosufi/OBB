4a5df72d90ad33602d0b901917b0458cc5d90471
o-yosufi <obaidullahyosafi@gmil.com>
2023-07-22 19:55:41 +0500

    new lessons

---
+++ python/boolean.py
@@ -0,0 +1,28 @@
+
+
+print(x+y)
+print(x-y)
+print(x/y)
+print(x*y)
+print(x%y)
+print(x//y)
+print(x**y)
+print(x>y)
+print(x>y)
+print(x>y)
+print(x>y)
+print(x>y)
+print(x>y)
+
+x = 5
+y = 6 
+x = 5
+
+x **= 3
+
+print(x)
+
+x = 5 
+x **= 3
+print(x)
+
---
+++ python/list.py
@@ -0,0 +1,17 @@
+# list
+afg = ["kabul","parwan","logar","gazni","kapeca"]
+
+print(afg)
+print(afg[2:4])
+print(afg[-1])
+print(afg[0:-1])
+print(afg[0:])
+print(afg[:6])
+if "logar" in afg:
+    print("logar")
+afg[2]= "takhar"
+
+# for loop
+time = [365, 24, 60]
+for y in time:
+    print(y / 2)
