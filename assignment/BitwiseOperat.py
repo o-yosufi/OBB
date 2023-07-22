b53cd10f5d0365c9d2079413fd2c967c73e6a304
o-yosufi <obaidullahyosafi@gmail.com>
2023-07-22 12:56:27 +0500

    operators added in assignment

---
+++ assignment/BitwiseOperat.py
@@ -0,0 +1,5 @@
+#bitwise operators
+print(7 & 2)
+print(3 & 5)
+print(10 | 5)
+print(4|8)
---
+++ assignment/MembOPerat.py
@@ -0,0 +1,12 @@
+# membership operators
+a = [5, 6, 3, 7, 9]
+b = [4, 'python', 2, 7,'simpleilrean', 9]
+
+print(5 in a)
+print(55 in a)
+print("python" in b)
+
+y = {1 : 'india', 2: 'USA'}
+
+print(1 in y)
+print('USA' in y)
